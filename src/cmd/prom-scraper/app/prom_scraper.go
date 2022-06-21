30e84c4a91b9ca5868faff0d3f4fb8438f3c1698
Carson Long <lcarson@vmware.com>
2022-06-21 14:16:00 -0700

    Ignore gosec where we need to set InsecureSkipVerify

--- src/cmd/prom-scraper/app/prom_scraper.go
+++ src/cmd/prom-scraper/app/prom_scraper.go
@@ -239,7 +239,7 @@ func (p *PromScraper) scrape(client *http.Client) scraper.MetricsGetter {
 
 func withSkipSSLValidation(skipSSLValidation bool) tlsconfig.ClientOption {
 	return func(tlsConfig *tls.Config) error {
-		tlsConfig.InsecureSkipVerify = skipSSLValidation
+		tlsConfig.InsecureSkipVerify = skipSSLValidation //nolint:gosec
 		return nil
 	}
 }
